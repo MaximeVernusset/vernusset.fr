76666df5dec22f7f32f410010f8e41426dd8a8f2
Maxime Vernusset <maxime.vernusset@edu.ece.fr>
2018-11-27 21:57:59 +0100

    MàJ du CV : ajout stage OCD, suppression version pour export

--- cv/controller/cvController.php
+++ cv/controller/cvController.php
@@ -1,15 +1,10 @@
 <?php
 
 	define('ACTION', 'action');
-	define('ACTION_EXPORT', 'export');
 	
 	class cvController {		
 		
 		public function display() {
 			require 'view/cv.html';
-		}
-	
-		public function displayExport() {
-			require 'view/cvExport.html';
 		}
 	}
